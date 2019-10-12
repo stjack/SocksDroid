29bd1ad8db9a326775f5fd17993c2886fe9c0cd7
stjack <stjack1@gmail.com>
2019-10-12 07:52:41 -0700

    2019-10-12 - apk works!

--- app/src/main/build-jni.sh
+++ app/src/main/build-jni.sh
@@ -3,12 +3,12 @@
 rm -rf assets
 rm -rf ../../libs
 
-ndk-build
+/home/jack/Android/Sdk/ndk/20.0.5594570/ndk-build -B
 
-for p in armeabi-v7a arm64-v8a x86 mips; do
+for p in armeabi-v7a arm64-v8a; do
 	mkdir -p assets/$p
-	cp libs/$p/{tun2socks,pdnsd} assets/$p/
+	cp libs/$p/{tun2socks,pdnsd,libsystem.so} assets/$p/
 done
 
-rm -rf libs/*/{tun2socks,pdnsd}
+rm -rf libs/*/{tun2socks,pdnsd,libsystem.so}
 mv libs ../../
--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI      			:= armeabi-v7a arm64-v8a    ##mips x86
 APP_PLATFORM 			:= android-23
 ##APP_STL      			:= c++_static       ##stlport_static
-APP_STL      			:= c++_shared
+APP_STL      			:= c++_static
 NDK_TOOLCHAIN_VERSION 	:= clang
